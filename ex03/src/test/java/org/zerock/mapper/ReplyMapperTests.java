688609cf0593cea2834c5b94163f49cb67163aa5
user <user@DESKTOP-DHNEMPR>
2022-03-07 11:38:55 +0900

    p393 commit

--- ex03/src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ ex03/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -1,10 +1,15 @@
 package org.zerock.mapper;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -14,12 +19,68 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class ReplyMapperTests {
 	
+	private Long[] bnoArr = {1L,2L,3L,4L,5L};
+	
 	@Setter(onMethod_=@Autowired)
 	private ReplyMapper mapper;
 	
 	@Test
 	public void testMapper() {
 		log.info(mapper);
+	}
+	
+	@Test
+	public void testCreate() {
+		IntStream.rangeClosed(1, 10).forEach(i ->{
+			
+			ReplyVO vo = new ReplyVO();
+			
+			vo.setBno(bnoArr[i]);
+			vo.setReply("Test reply" + i);
+			vo.setReplyer("replyer"+i);
+			
+			mapper.insert(vo);
+		});
+	}
+	
+	@Test
+	public void testRead() {
+		
+		Long targetRno = 5L;
+		
+		ReplyVO vo = mapper.read(targetRno);
+		log.info(vo);
+	}
+	
+	@Test
+	public void testDelete() {
+		
+		Long targetRno = 5L;
+		
+		mapper.delete(targetRno);
+		
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+		Long targetRno = 10L;
+		ReplyVO vo = mapper.read(targetRno);
+		
+		vo.setReply("update reply");
+		int count = mapper.update(vo);
+		
+		log.info("update count: "+ count);
+		
+	}
+	
+	@Test
+	public void testList() {
+		
+		Criteria cri = new Criteria();
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
+		
+		replies.forEach(reply->log.info(reply));
 	}
 
 }
